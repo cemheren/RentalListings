40c3abb879b91683dea7de6f4156ade961cc41a6
cemheren <cemheren@gmail.com>
2017-03-05 23:30:31 -0800

    add number of dimensions as a parameter to the training data. Reduced loss almost 0.1.

--- sample_model.py
+++ sample_model.py
@@ -7,10 +7,11 @@ import pickle
 x1 = pickle.load(open('data/simple_train_inputs.pickle', 'rb'))
 y1 = pickle.load(open('data/simple_train_labels.pickle', 'rb'))
 
-hidden_size = 128;
+input_size = 4
+hidden_size = 128
 
 model = Sequential()
-model.add(Dense(output_dim=hidden_size, input_dim=3, activation='sigmoid'))
+model.add(Dense(output_dim=hidden_size, input_dim=input_size, activation='sigmoid'))
 model.add(Dropout(0.2))
 model.add(Dense(output_dim=hidden_size, input_dim=hidden_size, activation='sigmoid'))
 model.add(Dropout(0.2))
@@ -22,12 +23,12 @@ model.compile(optimizer='sgd',
 
 # train the model, iterating on the data in batches
 # of 32 samples
-model.fit(x1, y1, validation_split=0.2, nb_epoch=50, batch_size=64, class_weight={0: 1.0, 1: 3.0, 2: 9.0})
+model.fit(x1, y1, validation_split=0.2, nb_epoch=100, batch_size=64, class_weight={0: 1.0, 1: 3.0, 2: 9.0})
 
 print("-- TESTING...")
 for i in range(30):
     print("-")
-    q = model.predict(np.reshape(x1[i], (1, 3)))[0]
+    q = model.predict(np.reshape(x1[i], (1, input_size)))[0]
     q = np.argmax(q, axis=0)
     print("prediction = ", q)
     print("y = ", np.argmax(y1[i], axis=0))
