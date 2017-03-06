c5172e6c981ec081e9150a4d8b6c53365feb2d37
H. Birkan Yilmaz <fermat4214@gmail.com>
2017-03-07 06:24:20 +0900

    Create jsonHandler_hby.py
    Json Handler for preprocessing data
    
    This version has
    - bath,  bed,  interest,  price,  number_of_images, region(lat,long)
    
    Region_id is converted to categorical encoding (i.e., indicator vector)

---
+++ data/jsonHandler_hby.py
@@ -0,0 +1,63 @@
+import ijson
+import json
+import keras.utils.np_utils
+import pickle
+import numpy as np
+from RL_encoders import to_region_idx_encoding
+
+interestResolver = dict()
+interestResolver['medium'] = 1
+interestResolver['low'] = 0
+interestResolver['high'] = 2
+
+f = open('train.json', 'r')
+x = json.load(f)
+
+traininputs = []
+trainlabels = []
+
+bath = [v for v in x['bathrooms'].values()]
+traininputs.append(bath)
+
+bed = [v for v in x['bedrooms'].values()]
+traininputs.append(bed)
+
+# created = x['created']
+
+interest = [interestResolver[v] for v in x['interest_level'].values()]
+h = np.histogram(interest, bins=[0, 1, 2, 3], density=True)
+print(h)
+
+interest = keras.utils.np_utils.to_categorical(interest)
+trainlabels = interest
+
+price = [v/1000.0 for v in x['price'].values()]
+traininputs.append(price)
+
+
+number_of_images = [v.__len__()/10.0 for v in x['photos'].values()]
+traininputs.append(number_of_images)
+
+
+# LatLong ==> Region Id ==> Categorical Encoding  (extra 24 inputs)
+# ZERO means outside
+region_xids = to_region_idx_encoding(x)
+# I guess I need to convert it to categorical encoding
+region_xids = keras.utils.np_utils.to_categorical(region_xids)
+col_num = region_xids.shape[1]
+for ii in range(0,col_num):
+    traininputs.append(region_xids[:,ii].tolist())
+
+
+# using only   bath,  bed,  interest,  price,  number_of_images,  region(lat,long)   so far.
+
+pickle.dump(trainlabels, open('simple_train_labels_hby.pickle', 'wb'))
+pickle.dump(np.array(traininputs).T, open('simple_train_inputs_hby.pickle', 'wb'))
+
+# todo: pre process the data here. e.g. normalize fields, outlier detection, one hot encoding.
+# todo: some fields can be simplified, like images -> just take the number of images to begin with
+# todo: the data contains tags like 'pet friendly' these should effect the desirability of the apartment. find a way to encode them.
+# todo: display_address field should also be included. similar to prev todo. find a way to encode them.
+
+# for prefix, the_type, value in ijson.parse(open('test.json')):
+#     print prefix, the_type, value
