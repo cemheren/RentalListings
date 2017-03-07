33724f76baf3e2b48c3b9f955af3224dbbbb41b6
H. Birkan Yilmaz <fermat4214@gmail.com>
2017-03-07 11:29:47 +0900

    Update sample_model.py
    Integrated region data to model, now model has 28 inputs
    
    Changed class weights
    
    Added few more layers
    
    Increased batch_size to 128 (it becomes faster when we increase batch_size which is counter intuitive, is this the size of batches or number of batches so that when you increase it the size of each batch decreases, because Karpthy once said you should use as much as possible for the batch size that is depending on your GPU limitations)

--- sample_model.py
+++ sample_model.py
@@ -1,36 +1,46 @@
+import numpy as np
+import pickle
+
 import keras
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout
-import numpy as np
-import pickle
+
 
 x1 = pickle.load(open('data/simple_train_inputs.pickle', 'rb'))
 y1 = pickle.load(open('data/simple_train_labels.pickle', 'rb'))
 
-input_size = 4
+# Use New Handler to Save Data that has 28 fields not 4
+input_size = 28
 hidden_size = 128
 
 model = Sequential()
-model.add(Dense(output_dim=hidden_size, input_dim=input_size, activation='sigmoid'))
-model.add(Dropout(0.2))
-model.add(Dense(output_dim=hidden_size, input_dim=hidden_size, activation='sigmoid'))
-model.add(Dropout(0.2))
-model.add(Dense(output_dim=3, input_dim=hidden_size, activation='softmax'))
+model.add(Dense(output_dim=2*hidden_size, input_dim=input_size, init='glorot_normal', activation='tanh'))
+model.add(Dense(output_dim=hidden_size, input_dim=2*hidden_size, init='glorot_normal', activation='sigmoid'))
+model.add(Dropout(0.3))
+model.add(Dense(output_dim=hidden_size, input_dim=hidden_size, init='glorot_normal', activation='sigmoid'))
+model.add(Dropout(0.3))
+model.add(Dense(output_dim=hidden_size, input_dim=hidden_size, init='glorot_normal', activation='sigmoid'))
+model.add(Dropout(0.3))
+model.add(Dense(output_dim=hidden_size, input_dim=hidden_size, init='glorot_normal', activation='sigmoid'))
+model.add(Dropout(0.3))
+model.add(Dense(output_dim=hidden_size, input_dim=hidden_size, init='glorot_normal', activation='sigmoid'))
+model.add(Dropout(0.3))
+model.add(Dense(output_dim=3, input_dim=hidden_size, init='glorot_normal', W_regularizer='l1l2', activation='softmax'))
 
-model.compile(optimizer='sgd',
+model.compile(optimizer='adam',
               loss='binary_crossentropy',
-              metrics=['accuracy'])
+              metrics=['accuracy', 'categorical_accuracy', 'fbeta_score'])
 
-# train the model, iterating on the data in batches
-# of 32 samples
-model.fit(x1, y1, validation_split=0.2, nb_epoch=100, batch_size=64, class_weight={0: 1.0, 1: 3.0, 2: 9.0})
+# train the model, iterating on the data in batches // VERBOSE=2 for printing metrics
+model.fit(x1, y1, validation_split=0.2, nb_epoch=100, batch_size=128, class_weight={0: 1.0, 1: 2.1, 2: 4.2}, verbose=2)
 
-print("-- TESTING...")
-for i in range(30):
-    print("-")
-    q = model.predict(np.reshape(x1[i], (1, input_size)))[0]
-    q = np.argmax(q, axis=0)
-    print("prediction = ", q)
-    print("y = ", np.argmax(y1[i], axis=0))
-    print("x = ", x1[i])
-    print("-")
+
+# print("-- TESTING...")
+# for i in range(30):
+#     print("-")
+#     q = model.predict(np.reshape(x1[i], (1, input_size)))[0]
+#     q = np.argmax(q, axis=0)
+#     print("prediction = ", q)
+#     print("y = ", np.argmax(y1[i], axis=0))
+#     print("x = ", x1[i])
+#     print("-")
