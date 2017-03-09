c4c8a2a52ab8f6f51210765086f8ea01ae949a1d
cemheren <cemheren@gmail.com>
2017-03-09 01:43:10 -0800

    change to an empirically better model

--- data/jsonHandler.py
+++ data/jsonHandler.py
@@ -42,7 +42,7 @@ number_of_description_words = [v.__len__()/1000.0 for v in x['description'].valu
 traininputs.append(number_of_description_words)
 
 
-features_array = get_top_n_features(x, 20)
+features_array = get_top_n_features(x, 50)
 pickle.dump(features_array, open('features_array.pickle', 'wb'))
 
 for feature in features_array:
--- sample_model_evaluator.py
+++ sample_model_evaluator.py
@@ -11,21 +11,15 @@ ids = pickle.load(open('data/listing_ids.pickle', 'rb'))
 # y1 = pickle.load(open('data/simple_train_labels.pickle', 'rb'))
 
 # Use New Handler to Save Data that has 28 fields not 4
-input_size = 49
-hidden_size = 256
+input_size = 79
+hidden_size = 1024
 
 model = Sequential()
-model.add(Dense(output_dim=2 * hidden_size, input_dim=input_size, init='glorot_normal', activation='tanh'))
-model.add(Dense(output_dim=hidden_size, input_dim=2 * hidden_size, init='glorot_normal', activation='sigmoid'))
-# model.add(Dropout(0.5))
+model.add(Dense(output_dim=hidden_size, input_dim=input_size, init='glorot_normal', activation='tanh'))
+model.add(Dropout(0.3))
 model.add(Dense(output_dim=hidden_size, input_dim=hidden_size, init='glorot_normal', activation='sigmoid'))
-# model.add(Dropout(0.5))
-model.add(Dense(output_dim=hidden_size, input_dim=hidden_size, init='glorot_normal', activation='sigmoid'))
-# model.add(Dropout(0.5))
-model.add(Dense(output_dim=hidden_size, input_dim=hidden_size, init='glorot_normal', activation='sigmoid'))
-# model.add(Dropout(0.5))
-model.add(Dense(output_dim=hidden_size, input_dim=hidden_size, init='glorot_normal', activation='sigmoid'))
-# model.add(Dropout(0.5))
+model.add(Dropout(0.3))
+
 model.add(Dense(output_dim=3, input_dim=hidden_size, init='glorot_normal', W_regularizer='l1l2', activation='softmax'))
 
 model.compile(optimizer='adam',
@@ -35,7 +29,7 @@ model.compile(optimizer='adam',
 # train the model, iterating on the data in batches // VERBOSE=2 for printing metrics
 # model.fit(x1, y1, validation_split=0.2, nb_epoch=1000, batch_size=512, verbose=2)
 
-model.load_weights('sample_model.km')
+model.load_weights('sample_model_79.km')
 
 result = model.predict(x1, verbose=2)
 
@@ -48,7 +42,7 @@ for i in range(len(result)):
     line = str(current_id) + ',' + ",".join(map(str, r))
     submission_file.append(line)
 
-sub_file = open('submission_256_10k.txt', 'w')
+sub_file = open('submission_79.txt', 'w')
 for item in submission_file:
     sub_file.write("%s\n" % item)
 
