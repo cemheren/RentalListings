7d936e3fc4a175bbd6cf40a3df337e764978954e
H. Birkan Yilmaz <fermat4214@gmail.com>
2017-03-12 01:50:30 +0900

    Integrating Preprocessing - Need to discuss
    For integrating preprocessing I had to do some changes
    
    It may be better to talk on these and discuss the changes (I didn't like my current solution actually)

--- data/jsonHandler.py
+++ data/jsonHandler.py
@@ -4,50 +4,48 @@ import keras.utils.np_utils
 import pickle
 import numpy as np
 from RL_encoders import to_region_idx_encoding, get_top_n_features
+from RL_preprocessor import find_keep_mask_for_price, normalize_col
 
 interestResolver = dict()
+interestResolver['high'] = 0
 interestResolver['medium'] = 1
 interestResolver['low'] = 2
-interestResolver['high'] = 0
+
 
 
 f = open('train.json', 'r')
 x = json.load(f)
 
-traininputs = []
-trainlabels = []
-
-bath = [v for v in x['bathrooms'].values()]
-traininputs.append(bath)
-
-bed = [v for v in x['bedrooms'].values()]
-traininputs.append(bed)
-
-# created = x['created']
-
+###########################
+# Output CLASS Values
+###########################
 interest = [interestResolver[v] for v in x['interest_level'].values()]
-h = np.histogram(interest, bins=[0, 1, 2, 3], density=True)
-print(h)
+print(np.histogram(interest, bins=[0, 1, 2, 3], density=True))
+trainlabels = keras.utils.np_utils.to_categorical(interest)
 
-interest = keras.utils.np_utils.to_categorical(interest)
-trainlabels = interest
 
-price = [v/1000.0 for v in x['price'].values()]
-traininputs.append(price)
-
+###########################
+# Input Variables
+###########################
+# Numerical Columns
+bath = [v for v in x['bathrooms'].values()]
+bed = [v for v in x['bedrooms'].values()]
+price = [v for v in x['price'].values()]
 number_of_images = [v.__len__()/10.0 for v in x['photos'].values()]
-traininputs.append(number_of_images)
-
 number_of_description_words = [v.__len__()/1000.0 for v in x['description'].values()]
-traininputs.append(number_of_description_words)
 
 
+# INPUT Matrix  (EASY to manipulate as matrix (NDArray) for trimming and normalizing)
+input_matrix = np.column_stack( (bath, bed, price, number_of_images, number_of_description_words) )
+
+
+# Features
 features_array = get_top_n_features(x, 50)
 pickle.dump(features_array, open('features_array.pickle', 'wb'))
 
 for feature in features_array:
     f_input = [1 if feature[0] in v else 0 for v in x['features'].values()]
-    traininputs.append(f_input)
+    input_matrix = np.c_[input_matrix, f_input]
 
 # LatLong ==> Region Id ==> Categorical Encoding  (extra 24 inputs)
 # ZERO means outside
@@ -58,13 +56,29 @@ pickle.dump(region_xids, open('region_xids.pickle', 'wb'))
 region_xids = keras.utils.np_utils.to_categorical(region_xids)
 col_num = region_xids.shape[1]
 for ii in range(0, col_num):
-    traininputs.append(region_xids[:, ii].tolist())
+    input_matrix = np.c_[input_matrix, region_xids[:, ii].tolist()]
 
 
-# using only   bath,  bed,  interest,  price,  number_of_images,  region(lat,long)   so far.
+
+###########################
+# Actual Cleaning Part Starts
+###########################
+
+# Outlier DELETION on <input_matrix> and <trainlabels>
+price_keep_mask = find_keep_mask_for_price(price)
+
+
+input_matrix = input_matrix[price_keep_mask, :]
+trainlabels = trainlabels[price_keep_mask, :]
+
+# Normalize Column(s)
+_PRICE_COL_ = 2
+input_matrix_norm = normalize_col(input_matrix, _PRICE_COL_)
+
+
 
 pickle.dump(trainlabels, open('simple_train_labels.pickle', 'wb'))
-pickle.dump(np.array(traininputs).T, open('simple_train_inputs.pickle', 'wb'))
+pickle.dump(input_matrix, open('simple_train_inputs.pickle', 'wb'))
 
 # todo: pre process the data here. e.g. normalize fields, outlier detection, one hot encoding.
 # todo: some fields can be simplified, like images -> just take the number of images to begin with
